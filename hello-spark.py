fb5041004191ad5ad2d86364576e774512127e2f
David-Mtz-M <91239383+David-Mtz-M@users.noreply.github.com>
2025-03-19 23:51:39 -0600

    Update hello-spark.py

--- hello-spark.py
+++ hello-spark.py
@@ -1,11 +1,13 @@
 from pyspark.sql import SparkSession
 
 # Create a spark session
-spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+spark = SparkSession.builder.appName("HelloDavid").getOrCreate()
 
 # Use sql() to write a raw SQL query
-df = spark.sql("SELECT 'Hello World' as hello")
+df = spark.sql("SELECT 'Hello David!' as hello")
 
 # Print the dataframe
 df.show()
+
+# Write the result to a JSON file
 df.write.mode("overwrite").json("results")
